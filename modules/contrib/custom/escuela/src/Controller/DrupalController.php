9f3d3a690286089e2cf98cf80bed4d4d85613b2b
gabgustavo <gustavoavilabar@hotmail.com>
2025-08-25 15:46:54 -0500

    Render arrays

--- modules/contrib/custom/escuela/src/Controller/DrupalController.php
+++ modules/contrib/custom/escuela/src/Controller/DrupalController.php
@@ -7,6 +7,21 @@ use Symfony\Component\HttpFoundation\Response;
 class DrupalController extends ControllerBase {
 
   public function escuela() {
-    return new Response('Este es el controlador de escuela');
+    //return new Response('Este es el controlador de escuela');
+    $build = [];
+
+    $build['primero'] = [
+      '#markup' => $this->t('Hola este es mi primer controlador'),
+    ];
+
+    $build['segundo'] = [
+      '#markup' => $this->t('Este es un segundo texto para el controlador'),
+    ];
+
+    /* return [
+      '#markup' => $this->t('Este es el controlador de escuela'),
+    ]; */
+
+    return $build;
   }
 }
