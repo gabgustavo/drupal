2ad1f4d7ce33130dcfc6cebf09dbf58a440dbf95
gabgustavo <gustavoavilabar@hotmail.com>
2025-08-25 12:36:49 -0500

    activar modo desarrollo

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -810,9 +810,11 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
+//TODO: para desactivar el mode desarrollo comentar el sigiente if👇🏼
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
+
 $databases['default']['default'] = array (
   'database' => 'cms_drupal',
   'username' => 'root',
