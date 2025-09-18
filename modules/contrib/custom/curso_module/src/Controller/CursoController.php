ccdbc5f8a785c5b81e6697db85c21d38a3e32ed4
gabgustavo <gustavoavilabar@hotmail.com>
2025-09-18 16:49:29 -0500

    💡 plantillas

--- modules/contrib/custom/curso_module/src/Controller/CursoController.php
+++ modules/contrib/custom/curso_module/src/Controller/CursoController.php
@@ -20,9 +20,19 @@ class CursoController  extends  ControllerBase{
     ];
   }
 
-    public function homeManual($node) {
+  public function homeManual($node) {
     return [
-      '#markup' => $this->t('Hola este es mi primer controlador con node dinamico '.$node),
+      '#theme' => 'curso_plantilla',
+      '#etiqueta' => 'Curso de Drupal 9',
+      '#tipo' => 'Pagina basica',
+    ];
+  }
+
+  public function homeDinamicoDos(NodeInterface $node) {
+    return [
+      '#theme' => 'curso_plantilla',
+      '#etiqueta' => $node->label(),
+      '#tipo' => $node->bundle(),
     ];
   }
 }
