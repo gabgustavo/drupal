71f6f4c1b1005d4212993d9ff0958e323131a1e2
gabgustavo <gustavoavilabar@hotmail.com>
2025-09-18 16:19:00 -0500

    💡 render array

--- modules/contrib/custom/curso_module/src/Controller/CursoController.php
+++ modules/contrib/custom/curso_module/src/Controller/CursoController.php
@@ -7,7 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CursoController  extends  ControllerBase{
   public function home() {
-    return new Response('Este es el controlador del curso');
+    return [
+      #'#plain_text' => $this->t('Hola este es mi primer controlador del curso con plain text'),
+      '#markup' => $this->t('Hola este es mi primer controlador del curso con markup'),
+    ];
   }
 }
 
