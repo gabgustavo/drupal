0d4ec395fc21f24944a636982ce34fdb948043a9
gabgustavo <gustavoavilabar@hotmail.com>
2025-09-19 10:16:47 -0500

    💡 implementacion de servicios dentro de otros servicios

--- modules/contrib/custom/curso_module/src/Services/Repetir.php
+++ modules/contrib/custom/curso_module/src/Services/Repetir.php
@@ -1,8 +1,34 @@
 <?php
 namespace Drupal\curso_module\Services;
 
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Messenger\MessengerInterface;
+
 class Repetir {
+
+  /**
+  * @var MessengerInterface
+  */
+  private $messenger;
+
+  /**
+  * @var EntityTypeManager
+  */
+  private $entityTypeManager;
+
+  public function __construct(
+    MessengerInterface $messenger,
+    EntityTypeManager $entityTypeManager
+    ) {
+    $this->messenger = $messenger;
+    $this->entityTypeManager = $entityTypeManager;
+
+  }
+
   public function repetir($palabra, $cantidad = 50) {
-    return str_repeat($palabra, $cantidad);
+    $result = str_repeat($palabra, $cantidad);
+    $this->messenger->addWarning('Este es un mensaje de prueba desde el servicio repetir');
+
+    return $result;
   }
 }
